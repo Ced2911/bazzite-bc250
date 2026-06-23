5adb747d37e5a9e4160fcfc3c7698f4f1650d6b6
Ced2911 <Ced2911@users.noreply.github.com>
2026-06-23 18:15:42 +0200

    Modify rtw88.sh to use RTL88x2BU-Linux-Driver
    Updated the script to clone a different repository and modified the installation commands for the kernel driver.

--- files/scripts/rtw88.sh
+++ files/scripts/rtw88.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-rpm -q kernel
-uname -r
+# 1. Déterminer la version exacte du noyau présent dans l'image
+KERNELRELEASE=$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
+echo "Target Kernel Release: $KERNELRELEASE"
 
+# 2. Cloner le dépôt
 cd /tmp
-git clone https://github.com/lwfinger/rtw88
-cd rtw88
+git clone https://github.com/RinCat/RTL88x2BU-Linux-Driver
+cd RTL88x2BU-Linux-Driver
 
-KERNELRELEASE=$(rpm -q kernel | sed -e s/^kernel-//)
+# 3. Blacklister le pilote natif rtw88 qui entre en conflit
+mkdir -p /etc/modprobe.d
+echo "blacklist rtw88_8822bu" > /etc/modprobe.d/rtw8822bu.conf
 
-echo "kernel: $KERNELRELEASE"
-
-sed -i "/^KVER ?= /s/.*/KVER := $KERNELRELEASE/" Makefile
-
-make
-make install
-make install_fw
-cp rtw88.conf /etc/modprobe.d/
+# 4. Compiler et installer pour la bonne version du noyau
+# On passe KVER_HOST à Make pour lui injecter la version extraite via rpm
+make KVER_HOST="$KERNELRELEASE"
+make install KVER_HOST="$KERNELRELEASE"
